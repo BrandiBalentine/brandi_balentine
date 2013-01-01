7872d2e92cf41fb85e3904f8ebc482d4c1a8b997
Brandi Balentine <brandibalentine@gmail.com>
2012-12-31 21:48:32 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BrandiBalentine::Application.config.secret_token = '9a2216c43619f933e9de810a54d974e02c4c299b9efdadbab161cd2026831eb7c6573f25e4991866a32ddc7ecfef8372d914ba0c8e21c5331a903bb7701ed2f2'
